a2050f28772d921d6d949ecc0482b6c12553504b
Shen <uashen@gmail.com>
2017-04-22 15:40:19 -0700

    initial

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public int PLAYER_ID = 1;
+
+	protected Rigidbody2D rigidbody;
+	protected PlayerInput playerInput;
+	protected Vector2 velocity;
+
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D> ();
+		playerInput = GetComponent<PlayerInput> ();
+	}
+
+	void Update () {
+		Debug.Log (playerInput.aimVector);
+	}
+}
---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	public bool isFacingRight;
+	public Vector2 aimVector;
+	PlayerController controller;
+
+	float horizontal;
+	float vertical;
+
+	private string A_INPUT = "A_Keyboard_";
+	private string B_INPUT = "B_Keyboard_";
+	private string A_GAMEPAD = "A_";
+	private string B_GAMEPAD = "B_";
+	private string X_GAMEPAD = "X_";
+	private string HORIZONTAL_INPUT = "L_XAxis_Keyboard_";
+	private string VERTICAL_INPUT = "L_YAxis_Keyboard_";
+	private string HORIZONTAL_GAMEPAD = "L_XAxis_";
+	private string VERTICAL_GAMEPAD = "L_YAxis_";
+	private string START_INPUT = "Start_";
+
+	void Start () {
+		controller = GetComponent<PlayerController> ();
+
+		int playerId = controller.PLAYER_ID;
+		A_INPUT += playerId;
+		B_INPUT += playerId;
+		HORIZONTAL_INPUT += playerId;
+		VERTICAL_INPUT += playerId;
+		A_GAMEPAD += playerId;
+		B_GAMEPAD += playerId;
+		X_GAMEPAD += playerId;
+		HORIZONTAL_GAMEPAD += playerId;
+		VERTICAL_GAMEPAD += playerId;
+		START_INPUT += playerId;
+
+		aimVector = Vector2.zero;
+	}
+
+	void Update () {
+		horizontal = Input.GetAxisRaw(HORIZONTAL_INPUT) + Input.GetAxisRaw(HORIZONTAL_GAMEPAD);
+		vertical = Input.GetAxisRaw(VERTICAL_INPUT) + Input.GetAxisRaw(VERTICAL_GAMEPAD);
+		aimVector.x = horizontal;
+		aimVector.y = vertical;
+
+		if (aimVector == Vector2.zero) {
+			if (isFacingRight) {
+				aimVector = Vector2.right;
+			}
+			else {
+				aimVector = -Vector2.right;
+			}
+		}
+
+	}
+}
