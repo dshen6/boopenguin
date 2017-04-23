e2a3b05e7d576bee823fcc189c58e883a7b8c12b
Shen <uashen@gmail.com>
2017-04-23 16:48:48 -0700

    score

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+
+	private Text scoreText;
+
+	private int score = 10;
+
+	void OnEnable() {
+		PlayerController.OnPenguinFall += PenguinFall;
+	}
+
+	void OnDisable() {
+		PlayerController.OnPenguinFall -= PenguinFall;
+	}
+
+	void PenguinFall() {
+		score--;
+	}
+	// Use this for initialization
+	void Start () {
+		scoreText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = "" + score;
+	}
+}
