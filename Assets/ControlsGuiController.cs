28fd659898dce84255a40af6c34c7489668222cb
Shen <uashen@gmail.com>
2017-04-23 17:04:43 -0700

    font

--- Assets/ControlsGuiController.cs
+++ Assets/ControlsGuiController.cs
@@ -7,20 +7,24 @@ public class ControlsGuiController : MonoBehaviour {
 
 	public GameObject player;
 	private PlayerInput input;
+	private Camera camera;
 	Text text;
 	private bool isActive;
-	// Use this for initialization
+	private Canvas canvas;
+
 	void Start () {
 		input = player.GetComponent<PlayerInput>();
 		text = GetComponent<Text>();
+		canvas = GetComponentInParent<Canvas>();
+		camera = GetComponent<Camera>();
 		isActive = true;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		if (!isActive) {
 			return;
 		}
+		text.rectTransform.localPosition = GetScreenPosition(player.transform, canvas, camera) + Vector3.up * 30;
 		if (input.hasPressedAllButtons()) {
 			StartCoroutine(FadeTo(0.0f, .4f));
 		}
@@ -36,5 +40,16 @@ public class ControlsGuiController : MonoBehaviour {
 			yield return null;
 			isActive = false;
 		}
+	}
+
+	public static Vector3 GetScreenPosition(Transform transform, Canvas canvas, Camera cam)
+	{
+		Vector3 pos;
+		float width = canvas.GetComponent<RectTransform> ().sizeDelta.x;
+		float height = canvas.GetComponent<RectTransform > ().sizeDelta.y;
+		float x = Camera.main.WorldToScreenPoint (transform.position).x / Screen.width;
+		float y = Camera.main.WorldToScreenPoint (transform.position).y / Screen.height;
+		pos = new Vector3 (width * x - width / 2, y * height - height / 2); 
+		return pos;    
 	}
 }
